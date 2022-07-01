0255b8e9690bb600b7962f1612a22c097cd6ff0a
Henrique Melo <hacmelo@gmail.com>
2022-06-30 22:27:34 -0300

    Adicionando pontuacao ao jogo
    Co-authored-by: gabrielapivetta <gpivetta99@gmail.com>

--- src/Jogo/Jogo.hs
+++ src/Jogo/Jogo.hs
@@ -1,44 +1,58 @@
-module Jogo.Jogo(
-    rodaJogo
-    ) where
+module Jogo.Jogo
+  ( rodaJogo,
+  )
+where
 
-import Carta.Carta ( geraBaralho, Carta )
+import Carta.Carta (Carta, geraBaralho, getValue)
 import Tabuleiro.Tabuleiro
-    ( desenhaTabuleiro, pegaCoordenada, Coordenada, geraTabuleiro, pegaCarta )
+  ( Coordenada,
+    desenhaTabuleiro,
+    geraTabuleiro,
+    pegaCarta,
+    pegaCoordenada,
+  )
+import Pontuacao.Pontuacao (verificaEscolhas, getValorCartaByCoordenada)
 
 rodaJogo :: IO ()
 rodaJogo = do
-    let baralho = geraBaralho 3 []
-    let tabuleiro = geraTabuleiro baralho
-    rodada tabuleiro
-    fimJogo tabuleiro
+  let baralho = geraBaralho 3 []
+  let tabuleiro = geraTabuleiro baralho
+  rodada tabuleiro 0
+  fimJogo tabuleiro
 
 fimJogo :: [[Carta]] -> IO ()
 fimJogo tabuleiro = putStrLn "Fim do jogo."
 
 -- TODO rodada:
--- 
-rodada :: [[Carta]] -> IO ()
-rodada tabuleiro = do
-    desenhaTabuleiro tabuleiro
-    putStrLn "Escolha uma carta:"
-    escolha <- getLine
-    let tabuleiro2 = abreCarta tabuleiro escolha
-    desenhaTabuleiro tabuleiro2
-    putStrLn "Escolha uma carta:"
-    escolha2 <- getLine
-    let tabuleiro3 = abreCarta tabuleiro2 escolha2
-    desenhaTabuleiro tabuleiro3
-    -- let jogada2 = abreCarta (tabuleiro jogada1)
-    -- if valor (carta jogada1) == valor (carta jogada2)
-    -- then do
-    --     putStrLn "Você acertou"
-    --     rodada (tabuleiro jogada2)
-    -- else do
-    --     desenhaTabuleiro escondeCartas (tabuleiro jogada2)
-    --     putStrLn "Você errou"
+--
+rodada :: [[Carta]] -> Int -> IO ()
+rodada tabuleiro pontuacaoTotal = do
+  desenhaTabuleiro tabuleiro
+  putStrLn "Escolha uma carta:"
+  escolha <- getLine
+  let tabuleiro2 = abreCarta tabuleiro escolha
+  valorEscolha1 <- getValorCartaByCoordenada tabuleiro2 escolha
+  desenhaTabuleiro tabuleiro2
+  putStrLn "Escolha uma carta:"
+  escolha2 <- getLine
+  let tabuleiro3 = abreCarta tabuleiro2 escolha2
+  valorEscolha2 <- getValorCartaByCoordenada tabuleiro3 escolha2
+  desenhaTabuleiro tabuleiro3
+  pontuacaoTotal2 <- verificaEscolhas valorEscolha1 valorEscolha2 pontuacaoTotal
+  rodada tabuleiro3 pontuacaoTotal2
+
+
+-- let jogada2 = abreCarta (tabuleiro jogada1)
+-- if valor (carta jogada1) == valor (carta jogada2)
+-- then do
+--     putStrLn "Você acertou"
+--     rodada (tabuleiro jogada2)
+-- else do
+--     desenhaTabuleiro escondeCartas (tabuleiro jogada2)
+--     putStrLn "Você errou"
 
 abreCarta :: [[Carta]] -> String -> [[Carta]]
 abreCarta tabuleiro escolha = do
-    let coordenada = pegaCoordenada escolha
-    pegaCarta tabuleiro coordenada 
+  let coordenada = pegaCoordenada escolha
+  pegaCarta tabuleiro coordenada
+
